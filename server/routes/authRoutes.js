8eba18b87a3d2ec01de364df2a1287889eeb71fb
Oussama El Jounaidi <mroussama.eljounaidi@gmail.com>
2025-08-26 17:23:43 +0200

    add register page et data mysql

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,35 @@
 import express from 'express';
-// import {connection}  from '../lib/db';    
+import bcrypt from 'bcrypt';
+import connection from '../lib/db.js';
+
 const router = express.Router();
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
     console.log(firstName)
 
     if (!firstName || !lastName || !email || !password) {
         return res.status(400).json({ error: "Tous les champs sont obligatoires." });
     }
+    try {
+        // hash du mot de passe
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    res.json({ message: "Utilisateur enregistre avec success." });
+        // requête SQL
+        const sql = `INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)`;
+        connection.query(sql, [firstName, lastName, email, hashedPassword], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: "Erreur serveur." });
+            }
+            res.json({ message: "Utilisateur enregistré avec succès.", userId: result.insertId });
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Erreur lors de l'inscription." });
+    }
 });
 
 export default router;
+
+// Login
+
