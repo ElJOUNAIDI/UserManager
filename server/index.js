8739ac4e3251562d580fb8e07772f56bcdad75b5
Oussama El Jounaidi <mroussama.eljounaidi@gmail.com>
2025-08-26 13:39:59 +0200

    add base de donnees

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from "cors";
-const app = express();
 import dotenv from "dotenv";
+import authRoutes from './routes/authRoutes.js';
+
 
 dotenv.config();
-
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/auth', authRoutes);
 
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,16 @@
 import express from 'express';
+// import {connection}  from '../lib/db';    
 const router = express.Router();
 
 router.post('/register', (req, res) => {
-    res.send('register');
+    const { firstName, lastName, email, password } = req.body;
+    console.log(firstName)
+
+    if (!firstName || !lastName || !email || !password) {
+        return res.status(400).json({ error: "Tous les champs sont obligatoires." });
+    }
+
+    res.json({ message: "Utilisateur enregistre avec success." });
 });
 
 export default router;
